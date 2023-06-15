dc0f4758e792de499c771fce41ddbd1707bccd80
João P <132589767+joao-paulorr@users.noreply.github.com>
2023-06-15 17:31:56 +0000

    h

--- src/script.js
+++ src/script.js
@@ -1,2 +1,9 @@
 function iniciar() {
+    let bola = document.getElementById('bola')
+    bola.classList.add('movimento')
+}
+
+function parar() {
+    let bola = document.getElementById('bola')
+    bola.classList.remove('movimento')
 }
